af1bb74577e615fabc74930bc527abd63e988ea7
lauglez01 <lauragonzalez33312@gmail.com>
2024-11-28 08:35:45 +0100

    He eliminado el movie-ranks porque voy a hacerlo en un mismo proyecto. No voy a utilizar la API de rapidapi ya que gastaría muchos requests. He metido los datos en el db.json para hacer un fetch al json-server local.

--- localAPI/client/src/main.js
+++ localAPI/client/src/main.js
@@ -1,24 +1,22 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+/*const url = 'https://imdb-top-100-movies.p.rapidapi.com/';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'cbcaa37ed2msh714b94742876cb9p1baacdjsnfddcbe0ccf04',
+		'x-rapidapi-host': 'imdb-top-100-movies.p.rapidapi.com'
+	}
+};
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const postData = async() => {
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+  
+}
 
-setupCounter(document.querySelector('#counter'))
+postData();*/
+
